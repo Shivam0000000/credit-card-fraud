76df2f1d7b0d268cc96aa57f8cc89c4cabc266ce
Your NameShivam0000000 <shivam805556@gmail.com.com>
2023-11-16 16:06:40 +0530

    Data Ingestion

--- src/component/data_ingest.py
+++ src/component/data_ingest.py
@@ -1,1 +1,48 @@
 from src.logger import logging
+from src.exception import CustomException
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+import pandas as pd
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path=os.path.join('artifact','raw_data.csv')
+    train_data_path=os.path.join('artifact','train_data.csv')
+    test_data_path=os.path.join('artifact','test_data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            logging.info("Enter the DataIngestion")  
+
+            df=pd.read_csv('notebook/data/creditcard.csv')
+
+            os.makedirs(os.path.dirname(self.data_ingestion.train_data_path),exist_ok=True)
+
+            df.to_csv(self.data_ingestion.raw_data_path,index=False,header=True)
+
+            logging.info("Train Test Split")
+
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.data_ingestion.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.data_ingestion.test_data_path,index=False,header=True)
+
+            logging.info("Data Ingestion is completed")
+
+            return (self.data_ingestion.train_data_path ,self.data_ingestion.test_data_path)
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+
+
--- src/exception.py
+++ src/exception.py
@@ -10,9 +10,9 @@ def error_message_detail(error,error_detail:sys):
 
 
 
-class CCustomException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
